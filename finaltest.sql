b94a1be2af16899592989bf04ac044732bdc4f2c
nguyentuesdd <nguyentued@gmail.com>
2018-06-08 21:51:10 +0700

    Update database

--- finaltest.sql
+++ finaltest.sql
@@ -13,8 +13,8 @@ CREATE DATABASE finaltest
 drop table if exists universitys;
 create table universitys 
 (
-  id int primary key,
-  sid char(3) not null,
+  id int not null,
+  sid char(3) primary key not null,
   sname character varying(255) not null,
   saddress character varying(255),
   benchmark double precision /*Điểm chuẩn*/ not null,
@@ -25,6 +25,18 @@ create table universitys
   geom geometry(Point,4326)
   );
 
+-- Tạo bảng chứa các ngành trong trường đại học
+drop table if exists sectors;
+create table sectors
+(
+  idsector char(15),
+  sid char(3)  references universitys(sid),
+  namesector character varying(255),
+  quotasector int,
+  pointsector double precision,
+  constraint pk_sector primary key (idsector, sid)
+);
+
 -- Tạo procedure có chức năng insert dữ liệu vào bảng universitys
 drop function if exists st_add_universitys(int, char(3), character varying(255), character varying(255), double precision, int, character varying(80), double precision, double precision);
  create or replace function st_add_universitys(id int, sid char(3), sname character varying(255), saddress character varying(255),
@@ -34,7 +46,43 @@ drop function if exists st_add_universitys(int, char(3), character varying(255),
       insert into universitys values (id, sid, sname, saddress, benchmark, quota, website, lng, lat, ST_SetSRID(st_point(lng, lat), 4326));
     end;
     $$ language plpgsql;
-
-
 -- select * from universitys;
 -- select st_add_universitys(1, 'QSB', 'Đại học Bách Khoa TpHCM', '268 Lý Thường Kiệt, phường 14, quận 10, TP.Hồ Chí Minh', 20, 4330, 'aao.hcmut.edu.vn', 106.659306, 10.773277);
+
+-- Procedure có chức năng tìm ra các trường nằm trong bán kính bao nhiêu m (Dành cho câu 2)
+-- lng và lat là tọa độ tâm (tọa độ của người dùng hoặc tọa độ của địa chỉ người dùng nhập)
+-- radius là bán kính người dùng nhập
+drop function if exists st_search_univercities(double precision, double precision, double precision);
+create or replace function st_search_univercities(stlng double precision, stlat double precision, radius double precision)
+	returns void as $$
+	begin
+		select * from universitys where st_intersects(geom, ST_Buffer(ST_SetSRID(st_point(stlng, stlat), 4326), radius)) = true;
+	end;
+	$$ language plpgsql;
+
+-- Procedure có chức năng tìm ra các trường nằm trong bán kính bao nhiêu m với điểm sàn(Dành cho câu 3)
+-- lng và lat là tọa độ tâm (tọa độ của người dùng hoặc tọa độ của địa chỉ người dùng nhập)
+-- radius là bán kính người dùng nhập
+-- point là điểm người dùng nhập
+drop function if exists st_search_univercities_with_benchmark(double precision, double precision, double precision, double precision);
+create or replace function st_search_univercities_with_benchmark(stlng double precision, stlat double precision, radius double precision, point double precision)
+	returns void as $$
+	begin
+		select * from universitys where (st_intersects(geom, ST_Buffer(ST_SetSRID(st_point(stlng, stlat), 4326), radius)) = true) and (benchmark <= point);
+	end;
+	$$ language plpgsql;
+
+-- Procedure có chức năng tìm ra các trường nằm trong bán kính bao nhiêu m với chỉ tiêu(Dành cho câu 4)
+-- lng và lat là tọa độ tâm (tọa độ của người dùng hoặc tọa độ của địa chỉ người dùng nhập)
+-- radius là bán kính người dùng nhập
+-- num là chỉ tiêu người dùng nhập
+drop function if exists st_search_univercities_with_quota(double precision, double precision, double precision, int);
+create or replace function st_search_univercities_with_quota(stlng double precision, stlat double precision, radius double precision, num int)
+	returns void as $$
+	begin
+		select * from universitys where (st_intersects(geom, ST_Buffer(ST_SetSRID(st_point(stlng, stlat), 4326), radius)) = true) and (quota <= int);
+	end;
+	$$ language plpgsql;
+
+
+-- Insert các ngành, nhóm ngành vào bảng sector, Lưu ý: phải insert sau khi bảng universitys đã có dữ liệu
